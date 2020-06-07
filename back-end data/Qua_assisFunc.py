aa113dbbae0470e2d3e3c32e2faa4ddfa2403655
IMEVA <b06705039@ntu.edu.tw>
2020-06-07 20:27:13 +0800

    update Qua_assisFunc

--- back-end data/Qua_assisFunc.py
+++ back-end data/Qua_assisFunc.py
@@ -1,130 +1,97 @@
-# function 1
+# assistant func 
 
-def GetAllIdType(StudentList):
-    id_col_name  = ['身分別1','身分別2','身分別3','特殊身份別']
-    column       = [str(x) for i in range(len(id_col_name)) for x in StudentList[id_col_name[i]].tolist() if str(x)!='nan']
-    return list(set(column))
+def countBedNum(DormList):
+    BedNum = {'男性':0,'女性':0}
+    for index, row in DormList.iterrows():
+        if('男' in row['dormName'] and row['is_disability']==0):
+            BedNum['男性'] += 1
+        elif('女' in row['dormName'] and row['is_disability']==0):
+            BedNum['女性'] += 1
+    return BedNum
 
+def get_id_dict(id_list):
+    index_value   = 1
+    id_dict       = dict()
+    for i in range(len(id_list)):
+        for j in id_list[i]:
+            if(j not in id_dict.keys()):
+                id_dict[j]   =  i+1
+    return id_dict
 
+def get_audit_dict(id_dict):
+    audit_dict = dict()
+    for i in audit_list:
+        audit_dict[i] = id_dict[i]
+    return audit_dict
 
-# function 2
+def get_str2int(id_dict, audit_dict, StudentList):
+    
+    id_index                           = [0 for i in range(len(StudentList))]
+    willing_index                      = [0 for i in range(len(StudentList))]
+    for index, row in StudentList.iterrows():
+        # check if have special identity_type & audit status
+        for i in id_col_name:
+            if(str(row[i])!='nan' and row[i] in id_dict.keys() and id_dict[row[i]]>id_index[index]):
+                if(i == '特殊身份別' or id_dict[row[i]] in list(audit_dict.values())):
+                    if(row['審查狀態']=='通過'):
+                        id_index[index]    = id_dict[row[i]]
+                else:
+                    id_index[index]    = id_dict[row[i]]
+        # no special identity_type, check the habitation
+        idDictNum = max(id_dict.values())
+        if(id_index[index] == 0):
+            if(row['戶籍地']=='桃園市'):
+                id_index[index] = idDictNum + 2
+            elif(row['戶籍地'] in last_order_habitation):
+                id_index[index] = idDictNum + 3
+            else:
+                id_index[index] = idDictNum + 1
+        # willing_index assign
+        if(str(row['校內外意願'])!='nan'):
+            willing_index[index]   = willing_InOutCam[str(row['校內外意願'])]
+    StudentList['校內外意願'] = willing_index
+    StudentList['id_index'] = id_index
+    return StudentList
 
-def DivideDF(ordered_IdList, StudentList, DormList):
-    
-    StudentList = StudentList.drop(columns = Ori_ColumnToBeDrop)
-    BedNumDict = countBedNum(DormList)
-    
-    # get get_str2int
-    id_dict = get_id_dict(ordered_IdList)
-    audit_dict = get_audit_dict(id_dict)
-    StudentList = get_str2int(id_dict, audit_dict, StudentList)
-    
-    # divide in-out campus
-    StudentList = StudentList.sort_values(by = '校內外意願').reset_index(drop = True)
-    InCamNum = len(StudentList)-StudentList.groupby('校內外意願').count()['性別'][3]
-    
-    InCam_df = StudentList.iloc[:InCamNum,:]
-    InCam_df = InCam_df.sort_values(by = '性別').reset_index(drop = True)
-    InCam_df['資格'] = [2 if (row['id_index']==1 and row['是否需要安排身障房間']=='是') else 0 for index,row in InCam_df.iterrows()]
-    
-    # incampus divide boy-girl
-    GirlInCamNum = InCam_df.groupby(['性別']).size()['女性']
-    GirlInCam = InCam_df.iloc[:GirlInCamNum,:].sort_values(by='id_index').reset_index(drop=True)
-    BoyInCam = InCam_df.iloc[GirlInCamNum:,:].sort_values(by='id_index').reset_index(drop=True)
+def assign_qualificaiton(df,BedNumDict):
+    bedNum = BedNumDict[df.at[0,'性別']]
+    id_count = df.groupby('id_index')['id_index'].count().tolist()
+    curI = 0
+    while(bedNum>0 and len(id_count)>0):
+        if(bedNum-id_count[0]>=0):
+            for i in range(id_count[0]):
+                if(df.at[curI+i,'資格']==0):
+                    df.at[curI+i,'資格'] = 1
+                    bedNum -= 1
+        else:
+            RandomOrder = sample([i for i in range(id_count[0])],k=id_count[0]) if(id_count[0]<bedNum) else sample([i for i in range(id_count[0])],k=bedNum)
+            for i in RandomOrder:
+                df.at[curI+i,'資格'] = 1
+                bedNum -= 1
+                if(bedNum ==0 ):
+                    break
+        curI = curI + id_count[0]
+        id_count = id_count[1:] 
 
-    
-    # WaitDF
-    WaitDF = StudentList.iloc[InCamNum:,:]
-    
-    
-    # get qualification of boy&girl df
-    GirlInCam = dealWithPreference(assign_qualificaiton(GirlInCam,BedNumDict))
-    BoyInCam = dealWithPreference(assign_qualificaiton(BoyInCam,BedNumDict))
-    GirlInCam = GirlInCam.sort_values(by='資格').reset_index(drop=True)
-    BoyInCam = BoyInCam.sort_values(by='資格').reset_index(drop=True)
-    
-        
-    # All-Wait DF
-    QuaGirlGroup = GirlInCam.groupby('資格').count()
-    NoQuaGirlNum = QuaGirlGroup['性別'][0]
-    QuaBoyGroup = BoyInCam.groupby('資格').count()
-    NoQuaBoyNum = QuaBoyGroup['性別'][0]
-    WaitAllDf = [GirlInCam.iloc[:NoQuaGirlNum,:],BoyInCam.iloc[:NoQuaBoyNum],WaitDF]
-    WaitDF = pd.concat(WaitAllDf)
-    
-    
-    # Output Girl&Boy df
-    GirlInCam = GirlInCam.iloc[NoQuaGirlNum:,:].drop(columns = AlgorithmNeedDrop).sort_values(by='id_index').reset_index(drop=True)
-    BoyInCam = BoyInCam.iloc[NoQuaBoyNum:,:].drop(columns = AlgorithmNeedDrop).sort_values(by='id_index').reset_index(drop=True)
-    
+    return df
 
-    
-    # organize Wait df
-    WaitDF = WaitDF.drop(columns=Wait_Drop)
-    return BoyInCam, GirlInCam, WaitDF
-    
+def dealWithPreference(df):
+    PfCol = ['區域志願1', '區域志願2', '區域志願3']
+    for i, row in df.iterrows():
+        for j in PfCol:
+            if(str(row[j])=='nan'):
+                df.at[i,j] = '一般區域'
+    return df
 
-    
-# function3
-def GetOutputDF(id_orderList, BoyQua, GirlQua, StudentList, WaitDF):
-    
-    # Divide WaitDF => campus,BOT
-    WaitDF = WaitDF.sort_values('校內外意願')
-    WillGroupNum = WaitDF.groupby('校內外意願')
-    CampusNum = WillGroupNum.count()['性別'][1] + WillGroupNum.count()['性別'][2]
-    NotBotNum = len(WaitDF) - WillGroupNum.count()['性別'][2] - WillGroupNum.count()['性別'][3]
-    
-    Campus = WaitDF.iloc[:CampusNum,:].drop(columns = CampusWait_Drop_AsQua).sort_values('性別')
-    Bot = WaitDF.iloc[NotBotNum:,:]
-    
-    # organize Campus
-    Campus['資格'] = [0 for i in range(len(Campus))]
-    CampusGirlNum = Campus.groupby('性別')['性別'].count().tolist()[0]
-    CampusBoy = OrderAssign(Campus.iloc[CampusGirlNum:])
-    CampusGirl = OrderAssign(Campus.iloc[:CampusGirlNum])
-    
-    BoyQua['順位序號'] = [0 for i in range(len(BoyQua))]
-    GirlQua['順位序號'] = [0 for i in range(len(GirlQua))]
-    
-    CampusBoy = pd.concat([BoyQua,CampusBoy])
-    CampusGirl = pd.concat([GirlQua,CampusGirl])
-    
-    # drop and merge
-    CampusBoy = CampusBoy.drop(columns=Qua_Drop)
-    CampusGirl = CampusGirl.drop(columns=Qua_Drop)
-    StudentListMergeWithCampus = StudentList.drop(columns = Ori_DfDropForICampus)
-    CampusBoy = pd.merge(CampusBoy,StudentListMergeWithCampus,on=['學號'])
-    CampusGirl = pd.merge(CampusGirl,StudentListMergeWithCampus,on=['學號'])
-    
-    # BOT drop & merge & Divide => BotBoy, BotGirl
-    StudentListMergeWithBot = StudentList.drop(columns = StudentList_Drop_ForMapBot)
-    Bot = Bot.drop(columns=Bot_Drop_ForOutput)
-    Bot = pd.merge(Bot,StudentListMergeWithBot,on=['學號'])
-    
-    Bot = Bot.sort_values('性別')
-    BotGirlNum = Bot.groupby('性別')['性別'].count().tolist()[0]
-    BotBoy = OrderAssign(Bot.iloc[BotGirlNum:])
-    BotGirl = OrderAssign(Bot.iloc[:BotGirlNum])
-    
-    # id_index => str
-    id_IndexStr = dict()
-    for i in range(1,len(id_orderList)+1):
-        id_IndexStr[i] = '00000000000000'
-        for j in id_orderList[i-1]:
-            if len(j)<len(id_IndexStr[i]):
-                    id_IndexStr[i] = j
-    id_IndexStr[len(id_IndexStr)+1] = '北北基及桃園以外的縣市'
-    id_IndexStr[len(id_IndexStr)+1] = '桃園'
-    id_IndexStr[len(id_IndexStr)+1] = '北北基'
-    print(id_IndexStr)
-    
-    CampusBoy['id_index'] = [id_IndexStr[i] for i in CampusBoy['id_index']]
-    CampusGirl['id_index'] = [id_IndexStr[i] for i in CampusGirl['id_index']]
-    BotBoy['id_index'] = [id_IndexStr[i] for i in BotBoy['id_index']]
-    BotGirl['id_index'] = [id_IndexStr[i] for i in BotGirl['id_index']]
-    
-    
-    return CampusBoy, CampusGirl, BotBoy, BotGirl
-    
-    # 永久地址=>國籍
-    # id_index 轉回 身份別
+def OrderAssign(df):
+    df = df.sort_values('id_index').reset_index(drop = True)
+    id_count            = df.groupby('id_index')['id_index'].count().tolist()
+    curI                = 0
+    OnListNum           = 1
+    for idC in id_count:
+        RandomOrder     = sample([i for i in range(idC)],k=idC)
+        for i in RandomOrder:
+            df.at[curI+i,'順位序號'] = OnListNum
+            OnListNum           += 1
+        curI            = curI + idC
+    return df
