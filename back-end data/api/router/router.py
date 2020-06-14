773b5f80a98f3219261250c6d8147df98ab54967
shaoyu0966 <b06705028@ntu.edu.tw>
2020-06-14 12:07:30 +0800

    compute time, memory

--- back-end data/api/router/router.py
+++ back-end data/api/router/router.py
@@ -3,6 +3,8 @@ from flask import request
 from flask import jsonify
 from app import app
 
+import time
+import resource
 import sys
 import pandas as pd
 import numpy as np
@@ -15,23 +17,25 @@ from utility.save_result import *
 @app.route('/api/get_all_identities/', methods = ['POST'])
 def identityPool():
     if request.method == 'POST':
+        time_start = time.clock()
         print('request', request)
-        # print('request.json', request.json)
         student_data = request.json
         identity_pool = GetAllIdType(student_data)
+        time_elapsed = (time.clock() - time_start)
         print('identity_pool', identity_pool)
+        print('time_elapsed', time_elapsed)
+        print('memory usage', resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
         return jsonify(identity_pool)
 
 @app.route('/api/match/', methods = ['POST'])
 def result():
     if request.method == 'POST':
+        time_start = time.clock()
         print('request', request)
-        # print('request.json', request.json)
         student = request.json['student']
         beds = request.json['beds']
         priority = request.json['priority']
         BoyInQua, GirlInQua, WaitDF = DivideDF(priority, student, beds)
-        #still Work-in-progress
         BoyInQua, GirlInQua = Match(BoyInQua, GirlInQua, beds)
         CampusBoy, CampusGirl, BotBoy, BotGirl = GetOutputDF(priority, BoyInQua, GirlInQua, student, WaitDF)
         
@@ -41,6 +45,10 @@ def result():
             "men_BOT": BotBoy,
             "women_BOT": BotGirl,
         }
+
+        time_elapsed = (time.clock() - time_start)
+        print('time_elapsed', time_elapsed)
+        print('memory usage', resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
         return jsonify(result)
     
 @app.route('/api/save/', methods = ['POST'])
@@ -48,7 +56,6 @@ def save():
     if request.method == 'POST':
         print('request', request)
         file_path = request.json['file_path']
-        print(file_path)
         men_campus_dorm = request.json['men_campus_dorm']
         women_campus_dorm = request.json['women_campus_dorm']
         men_BOT = request.json['men_BOT']
